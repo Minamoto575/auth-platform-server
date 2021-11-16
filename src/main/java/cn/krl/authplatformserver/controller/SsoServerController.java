24b77a20c4d86103ffa3324464fe706477c77d95
kuang <911190218@qq.com>
2021-11-16 15:10:37 +0800

    feature:删除未登录返回的view

--- src/main/java/cn/krl/authplatformserver/controller/SsoServerController.java
+++ src/main/java/cn/krl/authplatformserver/controller/SsoServerController.java
@@ -38,14 +38,14 @@ public class SsoServerController {
     @Autowired
     private void configSso(SaTokenConfig cfg) {
         // 配置：未登录时返回的View
-        cfg.sso.setNotLoginView(
-            () -> {
-                String msg =
-                    "当前会话在SSO-Server端尚未登录，请先访问"
-                        + "<a href='/sso/doLogin?phone=17388888888&pwd=123456' target='_blank'> doLogin登录</a > "
-                        + "进行登录之后，刷新页面开始授权";
-                return msg;
-            });
+        // cfg.sso.setNotLoginView(
+        //     () -> {
+        //         String msg =
+        //             "当前会话在SSO-Server端尚未登录，请先访问"
+        //                 + "<a href='/sso/doLogin?phone=17388888888&pwd=123456' target='_blank'> doLogin登录</a > "
+        //                 + "进行登录之后，刷新页面开始授权";
+        //         return msg;
+        //     });
 
         // 配置：登录处理函数
         cfg.sso.setDoLoginHandle(
